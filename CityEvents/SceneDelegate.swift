4053bdaa4bbd199530c92a596eb084326b051b82
Aleksey Efimov <efimov.may@yandex.ru>
2024-06-08 16:57:01 +0300

    Initial Commit

---
+++ CityEvents/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  CityEvents
+//
+//  Created by Aleksey Efimov on 08.06.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+	
+	var window: UIWindow?
+	
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+		guard let windowScene = (scene as? UIWindowScene) else { return }
+		window = UIWindow(windowScene: windowScene)
+		
+		let ViewController = ViewController()
+		
+		window?.rootViewController = ViewController
+		window?.makeKeyAndVisible()
+	}
+}
+
---
+++ CityEvents/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  CityEvents
+//
+//  Created by Aleksey Efimov on 08.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .cyan
+	}
+}
+
